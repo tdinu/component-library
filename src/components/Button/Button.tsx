420aa113d87836afba9b39648f195c7828232c02
tdinu <dinutitiana@yahoo.com>
2024-03-23 14:19:02 +0100

    init repo

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,9 @@
+import React, { FC, ReactNode } from 'react';
+interface Props {
+  children: ReactNode | ReactNode[];
+}
+const Button: FC<Props> = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default Button;
